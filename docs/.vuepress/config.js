c0aa5263e5b2fa62d7fc2c8c74afc3ef62c6b382
QiShao <474021214@qq.com>
2018-09-18 00:53:05 +0800

    rebuild

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,12 +2,12 @@ module.exports = {
   title: 'css tricks',
   description: 'some css tricks, 一些常用的css样式, 一点css技巧',
   dest: './dist',    // 设置输出目录
-  base:'./',
-  repo: 'https://github.com/QiShaoXuan/css_tricks', // 添加 github 链接
+  base:'/css_tricks/',
+  repo: 'https://qishaoxuan.github.io/css_tricks/', // 添加 github 链接
   themeConfig: {
-    nav: [
-       { text: 'github', link: '/' },
-    ],
+    // nav: [
+    //    { text: 'github', link: '/' },
+    // ],
     sidebar: [
       ['/', 'Home'],
       ['/reset/', 'reset'],
