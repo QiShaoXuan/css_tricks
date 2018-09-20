02e7aa80fbfbabeb4a210771670087e5207517b9
lyyleyuanyuan <lyyleyuanyuan@163.com>
2018-09-20 14:33:13 +0800

    新增 垂直居中的方法

---
+++ docs/.vuepress/components/verticalCenterB.vue
@@ -0,0 +1,40 @@
+<style scoped lang="scss">
+.parentElement {
+  background: blue;
+  width: 100%;
+  height: 100px;
+  display: table;
+  .childElement {
+    // background: yellow;
+    width: 20%;
+    height: 30%;
+    display: table-cell;
+    vertical-align: middle;
+    color: #fff;
+
+  }
+  // .grandsonElement {
+  //   color: red;
+  //   width: 20%;
+  //   height: 30%;
+  // }
+}
+</style>
+
+<template>
+  <div class="parentElement">
+    <div class="childElement">
+      childElement内容
+      <div class="grandsonElement">grandsonElement内容</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "verticalCenterB",
+  data() {
+    return {};
+  }
+};
+</script>
