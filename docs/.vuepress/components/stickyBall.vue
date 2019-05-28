e604f7f28f56bf045dce43b7a294668bf7520c0f
QiShaoXuan <474021214@qq.com>
2019-05-28 19:27:19 +0800

    add sticky-ball

---
+++ docs/.vuepress/components/stickyBall.vue
@@ -0,0 +1,123 @@
+<style scoped lang="scss">
+.effect {
+  width: 100%;
+  height: 100%;
+  padding-top: 50px;
+  filter: blur(10px) contrast(30);
+  /*filter:contrast(10);*/
+  background: #fff;
+}
+.blackball {
+  width: 100px;
+  height: 100px;
+  background: black;
+  padding: 10px;
+  border-radius: 50%;
+  margin: 0 auto;
+  z-index: 1;
+}
+.redball {
+  width: 60px;
+  height: 60px;
+  background: #f00;
+  padding: 10px;
+  border-radius: 50%;
+  position: absolute;
+  top: 70px;
+  left: 50px;
+  z-index: 2;
+  left: 50%;
+  /*animation:rball 16s infinite;*/
+}
+@keyframes rball {
+  0%,
+  100% {
+    left: 35px;
+    width: 60px;
+    height: 60px;
+  }
+  4%,
+  54% {
+    width: 60px;
+    height: 60px;
+  }
+  10%,
+  60% {
+    width: 50px;
+    height: 70px;
+  }
+  20%,
+  70% {
+    width: 60px;
+    height: 60px;
+  }
+  34%,
+  90% {
+    width: 70px;
+    height: 50px;
+  }
+  41% {
+    width: 60px;
+    height: 60px;
+  }
+  50% {
+    left: calc(100% - 95px);
+    width: 60px;
+    height: 60px;
+  }
+}
+
+.blurfield {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  filter: blur(10px) contrast(30);
+}
+.ball1,
+.ball2 {
+  background: red;
+  width: 80px;
+  height: 80px;
+  animation: ball 10s infinite;
+  border-radius: 100%;
+}
+.ball2 {
+  background: blue;
+  animation-delay: -2s;
+}
+@keyframes ball {
+  0% {
+    transform: translateX(-100px);
+  }
+
+  50% {
+    transform: translateX(100px);
+  }
+  100% {
+    transform: translateX(-100px);
+  }
+}
+</style>
+
+<template>
+ <div class="sticky-container">
+   <div class="blurfield">
+     <div class="ball1"></div>
+     <div class="ball2"></div>
+   </div>
+   <!--<div class="effect">-->
+     <!--<div class="blackball"></div>-->
+     <!--<div class="redball"></div>-->
+   <!--</div>-->
+ </div>
+
+</template>
+
+<script>
+export default {
+  name: "sticky-ball",
+  data() {
+    return {};
+  }
+};
+</script>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,105 +1,117 @@
 module.exports = {
-  title: 'CSS Tricks',
-  description: 'some CSS tricks, 一些常用的 CSS 样式, 一点 CSS 技巧',
-  dest: './dist', // 设置输出目录
-  base: '/css_tricks/',
+  title: "CSS Tricks",
+  description: "some CSS tricks, 一些常用的 CSS 样式, 一点 CSS 技巧",
+  dest: "./dist", // 设置输出目录
+  base: "/css_tricks/",
   head: [
-    ['link', { rel: 'icon', href: '/images/favicon.png' }],
-    ['meta', { name: 'theme-color', content: '#00adb5' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#00adb5' }],
-    ['meta', {name:'description', itemprop: 'description', content: 'some CSS tricks, 一些常用的 CSS 样式, 一点 CSS 技巧' }],
-    ['meta', { itemprop: 'name', content: 'CSSTricks' }],
-    ['meta', { itemprop: 'image', content: '/css_tricks/images/favicon.png' }],
+    ["link", { rel: "icon", href: "/images/favicon.png" }],
+    ["meta", { name: "theme-color", content: "#00adb5" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#00adb5" }],
+    [
+      "meta",
+      {
+        name: "description",
+        itemprop: "description",
+        content: "some CSS tricks, 一些常用的 CSS 样式, 一点 CSS 技巧"
+      }
+    ],
+    ["meta", { itemprop: "name", content: "CSSTricks" }],
+    ["meta", { itemprop: "image", content: "/css_tricks/images/favicon.png" }]
   ],
-  repo: 'https://github.com/QiShaoXuan/css_tricks', // 添加 github 链接
+  repo: "https://github.com/QiShaoXuan/css_tricks", // 添加 github 链接
   themeConfig: {
     themeConfig: {
-      lastUpdated: 'Last Updated',
+      lastUpdated: "Last Updated"
     },
-    nav: [{
-        text: 'js tricks',
-        link: 'https://qishaoxuan.github.io/js_tricks/'
+    nav: [
+      {
+        text: "js tricks",
+        link: "https://qishaoxuan.github.io/js_tricks/"
       },
       {
-        text: 'blog',
-        link: 'https://qishaoxuan.github.io/blog/'
+        text: "blog",
+        link: "https://qishaoxuan.github.io/blog/"
       },
       {
-        text: 'GitHub',
-        link: 'https://github.com/QiShaoXuan/css_tricks'
-      },
+        text: "GitHub",
+        link: "https://github.com/QiShaoXuan/css_tricks"
+      }
     ],
     sidebar: [
       {
-        title: '工具',
+        title: "工具",
         collapsable: false,
         children: [
-          ['/createTriangle/', '三角形生成器'],
-          ['/sector/','扇形生成器'],
-        ],
-      },
-      {
-        title: '属性',
-        collapsable: false,
-        children: [
-          ['/filter/', '滤镜 filter'],
-          ['/flexbox/', 'flexbox'],
-          ['/grid/','grid'],
-          ['/object/','object-fit, object-position'],
-          ['/mixBlendMode/','mix-blend-mode'],
-          ['/list/','list-style']
+          ["/createTriangle/", "三角形生成器"],
+          ["/sector/", "扇形生成器"]
         ]
       },
       {
-        title: '动',
+        title: "属性",
         collapsable: false,
         children: [
-          ['/poptip/','提示气泡'],
-          ['/accordion/', '手风琴'],
-          ['/hover/', '悬停'],
-          ['/loading/', '加载动画'],
-          ['/spring/', '弹簧动画'],
-          ['/parabola/','抛物线'],
-          ['/notCSS/ripple','涟漪'],
-          ['/notCSS/wave','波浪']
+          ["/filter/", "滤镜 filter"],
+          ["/flexbox/", "flexbox"],
+          ["/grid/", "grid"],
+          ["/object/", "object-fit, object-position"],
+          ["/mixBlendMode/", "mix-blend-mode"],
+          ["/list/", "list-style"]
         ]
       },
       {
-        title: '静',
+        title: "动",
         collapsable: false,
         children: [
-          ['/reset/', '清除默认样式'],
-          ['/scrollTemp/', '滚动条样式'],
-          ['/media/', 'media 参考'],
-          ['/landscapeScreen/', '强制横屏'],
-          ['/cursor/', '手型'],
-          ['/shadow/', '投影'],
-          ['/remDpi/', 'rem, dpi 设置'],
-          ['/paper/','纸页效果'],
+          ["/poptip/", "提示气泡"],
+          ["/accordion/", "手风琴"],
+          ["/hover/", "悬停"],
+          ["/loading/", "加载动画"],
+          ["/spring/", "弹簧动画"],
+          ["/stickyBall/", "粘性球"],
+          ["/parabola/", "抛物线"],
+          ["/notCSS/ripple", "涟漪"],
+          ["/notCSS/wave", "波浪"]
         ]
       },
       {
-        title: '技巧',
+        title: "静",
         collapsable: false,
         children: [
-          ['/verticalMiddle/', '垂直居中'],
-          ['/vertical/', '消除图片间隙 & 图文居中'],
-          ['/bottom/', '底部自适应 Sticky Footer'],
-          ['/triangle/', '三角形边框'],
-          ['/ellipsis/', '文字超出显示省略号'],
-          ['/lineBreak/', '插入换行'],
-          ['/glass/', '毛玻璃效果'],
-          ['/align/','参数列表自适应对齐'],
-          ['/positionWidth/','绝对定位宽度自动撑开'],
-          ['/longText/','长文本折行'],
-          ['/textShadow/','文字特效'],
-          ['/tab/','tab 栏底部动画'],
-          ['/hollowOut/','镂空效果'],
-          ['/polygonLayout/','多边形布局']
+          ["/reset/", "清除默认样式"],
+          ["/scrollTemp/", "滚动条样式"],
+          ["/media/", "media 参考"],
+          ["/landscapeScreen/", "强制横屏"],
+          ["/cursor/", "手型"],
+          ["/shadow/", "投影"],
+          ["/remDpi/", "rem, dpi 设置"],
+          ["/paper/", "纸页效果"]
         ]
       },
+      {
+        title: "技巧",
+        collapsable: false,
+        children: [
+          ["/verticalMiddle/", "垂直居中"],
+          ["/vertical/", "消除图片间隙 & 图文居中"],
+          ["/bottom/", "底部自适应 Sticky Footer"],
+          ["/triangle/", "三角形边框"],
+          ["/ellipsis/", "文字超出显示省略号"],
+          ["/lineBreak/", "插入换行"],
+          ["/glass/", "毛玻璃效果"],
+          ["/align/", "参数列表自适应对齐"],
+          ["/positionWidth/", "绝对定位宽度自动撑开"],
+          ["/longText/", "长文本折行"],
+          ["/textShadow/", "文字特效"],
+          ["/tab/", "tab 栏底部动画"],
+          ["/hollowOut/", "镂空效果"],
+          ["/polygonLayout/", "多边形布局"]
+        ]
+      }
     ]
   }
-}
+};
